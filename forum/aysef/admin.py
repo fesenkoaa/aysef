269d4f23fd819e934cf7c37c7ad82af2a580cf95
fesenkoaa <fesenko.a@icloud.com>
2021-11-09 16:50:24 +0100

    Added models.py & changed admin.py

--- forum/aysef/admin.py
+++ forum/aysef/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'auth', 'theme', 'title', 'created_at', 'updated_at')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    list_filter = ('auth', 'theme')
+
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'theme')
+    list_display_links = ('theme',)
+
+
+class SupportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number', 'message', 'time')
+    list_display_links = ('number',)
+    search_fields = ('number',)
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Theme, ThemeAdmin)
+admin.site.register(Support, SupportAdmin)
--- forum/aysef/urls.py
+++ forum/aysef/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
 
     path('forum/', Forum.as_view(), name='forum'),
     path('forum/themes/', Themes.as_view(), name='themes'),
-    path('forum/unique_number/<slug:unique_number>/', TheTheme.as_view(), name='theme'),  # -
+    path('forum/theme/<slug:unique_key>/', TheTheme.as_view(), name='theme'),  # List of articles on the theme
     path('forum/add-theme/', AddTheme.as_view(), name='add_theme'),  # admin
-    path('forum/delete-theme/tun/<int:unique_number>/', DeleteTheme.as_view(), name='delete_theme'),  # admin  # -
+    path('forum/delete-theme/tun/<int:unique_key>/', DeleteTheme.as_view(), name='delete_theme'),  # admin  # -
     path('forum/<int:unique_number>', TheArticle.as_view(), name='article'),  # -
     path('forum/add-article/', AddArticle.as_view(), name='add_article'),
-    path('forum/edit-article/<int:unique_number>', EditArticle.as_view(), name='edit_article'),  # -
-    path('forum/delete-article/<int:unique_number>', DeleteArticle.as_view(),  # -
+    path('forum/edit-article/<int:unique_key>', EditArticle.as_view(), name='edit_article'),  # -
+    path('forum/delete-article/<int:unique_key>', DeleteArticle.as_view(),  # -
          name='delete_article'),
 
     path('registration/', Reg.as_view(), name='registration'),
